f56c2bc9aa706469bd53796f015dbf8e94665ade
Eddie Fong <fonged@oocl.com>
2018-11-21 14:42:08 +0800

    done grouping_count_spec

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,18 @@
 'use strict';
 
+function onlyUnique(value, index, self) { 
+  return self.indexOf(value) === index;
+}
+
 function grouping_count(collection) {
 
   //implement here
+  let result = {}
+  collection.filter(onlyUnique).forEach(element => {
+    result[element] = collection.filter((x)=>(x === element)).length;
+  });
+  return result;
 }
 
 module.exports = grouping_count;
+
