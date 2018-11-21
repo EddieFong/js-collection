b67ea5c7e49d38c710999201c20b2b654d274840
Eddie Fong <fonged@oocl.com>
2018-11-21 14:08:05 +0800

    done choose_even

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,22 @@
 'use strict';
+function divBy(element, div) {
+  return (element % div) === 0;
+}
 
 function choose_even(collection) {
 
+  // let result = []
+  // collection.forEach(element => {
+  //   if ( divBy(element, 2)){
+  //     result.push(element)
+  //   }
+  // });
   //implement here
+  // return result;
+  return collection.filter((x)=>divBy(x,2));
+
 }
 
 module.exports = choose_even;
+
+
