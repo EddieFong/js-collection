772602c549c4846e590beeea5d13ccb1e533a230
Eddie Fong <fonged@oocl.com>
2018-11-21 14:51:07 +0800

    using for loop

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -2,12 +2,17 @@
 function map_to_even(collection){
 
   // return collection.map((x) => x*2);
-  
-  let result = []
-  collection.forEach(element => {
-    result.push(element * 2)
-  });
-  return result;
+
+  // let result = []
+  // collection.forEach(element => {
+  //   result.push(element * 2)
+  // });
+  // return result;
+
+  for (var i = 0; i < collection.length; i++) {
+    collection[i] = collection[i] * 2
+  }
+  return collection
 
 }
 module.exports = map_to_even;
--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -3,11 +3,17 @@ var map_to_four_multiples_add_one = function(collection){
 
   // return collection.map((x) => x*4+1);
 
-  let result = []
-  collection.forEach(element => {
-    result.push(element * 4 + 1)
-  });
-  return result;
+  // let result = []
+  // collection.forEach(element => {
+  //   result.push(element * 4 + 1)
+  // });
+  // return result;
+
+  for (var i = 0; i < collection.length; i++) {
+    collection[i] = collection[i] * 2
+  }
+  return collection
+
 };
 
 module.exports = map_to_four_multiples_add_one;
