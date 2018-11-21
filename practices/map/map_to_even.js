c994a357aa51862f80280e27cfc865bfe1deedcb
Eddie Fong <fonged@oocl.com>
2018-11-21 15:06:29 +0800

    extract fn for map

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,7 +1,11 @@
 'use strict';
 function map_to_even(collection){
 
-  // return collection.map((x) => x*2);
+  const mapFn = function(x){
+    return x * 2
+  }
+
+  return collection.map(mapFn);
 
   // let result = []
   // collection.forEach(element => {
@@ -9,10 +13,10 @@ function map_to_even(collection){
   // });
   // return result;
 
-  for (var i = 0; i < collection.length; i++) {
-    collection[i] = collection[i] * 2
-  }
-  return collection
+  // for (var i = 0; i < collection.length; i++) {
+  //   collection[i] = collection[i] * 2
+  // }
+  // return collection
 
 }
 module.exports = map_to_even;
--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,7 +1,12 @@
 'use strict';
+
+const mapFn = function(x){
+  return x * 4 + 1
+}
+
 var map_to_four_multiples_add_one = function(collection){
 
-  // return collection.map((x) => x*4+1);
+   return collection.map(mapFn);
 
   // let result = []
   // collection.forEach(element => {
@@ -9,10 +14,10 @@ var map_to_four_multiples_add_one = function(collection){
   // });
   // return result;
 
-  for (var i = 0; i < collection.length; i++) {
-    collection[i] = collection[i] * 2
-  }
-  return collection
+  // for (var i = 0; i < collection.length; i++) {
+  //   collection[i] = collection[i] * 4 + 1
+  // }
+  // return collection
 
 };
 
