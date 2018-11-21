336806c799282b34d24663b3a43ee56fc3ef94be
Eddie Fong <fonged@oocl.com>
2018-11-21 14:19:54 +0800

    done choose_multiples_of_three

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,8 +1,21 @@
 'use strict';
+function divBy(element, div) {
+  return (element % div) === 0;
+}
 
 function choose_multiples_of_three(collection) {
 
   //implement here
+  
+  // let result = []
+  // collection.forEach(element => {
+  //   if ( divBy(element, 2)){
+  //     result.push(element)
+  //   }
+  // });
+
+  // return result;
+  return collection.filter((x)=>divBy(x,3));
 }
 
 module.exports = choose_multiples_of_three;
