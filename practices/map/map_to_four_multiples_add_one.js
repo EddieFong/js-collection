da446a4f6c1b24d465c395a0a6fea7a84e6e4835
Eddie Fong <fonged@oocl.com>
2018-11-21 14:49:24 +0800

    using foreach

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,13 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+
+  // return collection.map((x) => x*4+1);
+
+  let result = []
+  collection.forEach(element => {
+    result.push(element * 4 + 1)
+  });
+  return result;
 };
 
 module.exports = map_to_four_multiples_add_one;
